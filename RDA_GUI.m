9ddfccaf325b36a7204b664d89044d98215b6929
Benoit BERANGER <benoit.beranger@icm-institute.org>
2016-10-05 13:21:00 +0200

    stream GUI ok, need to program the data reception

--- RDA_GUI.m
+++ RDA_GUI.m
@@ -63,21 +63,38 @@ if isempty(figPtr) % Create the figure
         'Tooltip','IP adress',...
         'Callback',@edit_Adress_Callback);
     
-    % On/Off
-    t_onoff.x = e_adr.x + e_adr.w + 0.05;
-    t_onoff.w = e_adr.w;
-    t_onoff.y = e_adr.y;
-    t_onoff.h = e_adr.h;
-    t_onoff.tag = 'toggle_OnOff';
-    handles.(t_onoff.tag) = uicontrol(figHandle,...
+    % Connecion
+    t_con.x = e_adr.x + e_adr.w + 0.05;
+    t_con.w = e_adr.w;
+    t_con.y = e_adr.y;
+    t_con.h = e_adr.h;
+    t_con.tag = 'toggle_Connection';
+    handles.(t_con.tag) = uicontrol(figHandle,...
         'Style','toggle',...
-        'Tag',t_onoff.tag,...
+        'Tag',t_con.tag,...
         'Units', 'Normalized',...
-        'Position',[t_onoff.x t_onoff.y t_onoff.w t_onoff.h],...
+        'Position',[t_con.x t_con.y t_con.w t_con.h],...
         'BackgroundColor',handles.buttonBGcolor,...
-        'String','Stream Off',...
-        'Tooltip','Switch On/Off the stream',...
-        'Callback',@toggle_OnOff_Callback);
+        'String','Connect',...
+        'Tooltip','Switch On/Off TCPIP connection',...
+        'Callback',@toggle_Connection_Callback);
+    
+    % Stream
+    t_stream.x = t_con.x + t_con.w + 0.05;
+    t_stream.w = e_adr.w;
+    t_stream.y = e_adr.y;
+    t_stream.h = e_adr.h;
+    t_stream.tag = 'toggle_Stream';
+    handles.(t_stream.tag) = uicontrol(figHandle,...
+        'Style','toggle',...
+        'Tag',t_stream.tag,...
+        'Units', 'Normalized',...
+        'Position',[t_stream.x t_stream.y t_stream.w t_stream.h],...
+        'BackgroundColor',handles.buttonBGcolor,...
+        'String','Stream',...
+        'Tooltip','Switch On/Off the data streaming',...
+        'Callback',@toggle_Stream_Callback,....
+        'Visible','Off');
     
     
     %% End of opening
@@ -118,50 +135,6 @@ end % function
 
 %% GUI Functions
 
-% *************************************************************************
-function toggle_OnOff_Callback(hObject, eventdata)
-
-handles = guidata(hObject);
-
-switch get(hObject,'Value')
-    
-    case 1
-        
-        recorderip = get(handles.edit_Adress,'String');
-        
-        fprintf('Trying to connect to : %s ... \n',recorderip)
-        
-        % Establish connection to BrainVision Recorder Software 32Bit RDA-Port
-        % (use 51234 to connect with 16Bit Port)
-        con = pnet('tcpconnect', recorderip, 51244);
-        
-        % Check established connection and display a message
-        status = pnet(con,'status');
-        if status > 0
-            disp('connection established');
-        elseif status == -1
-            set(hObject,'Value',0)
-            error('connection FAILED')
-        end
-        
-        set(hObject,'String','Steam ON')
-        set(hObject,'BackgroundColor',[0.5 0.5 1])
-        
-    case 0
-        
-        % Close all open socket connections
-        pnet('closeall');
-        
-        % Display a message
-        disp('connection closed');
-        
-        set(hObject,'String','Steam off')
-        set(hObject,'BackgroundColor',handles.buttonBGcolor)
-        
-end
-
-end % function
-
 
 % *************************************************************************
 function edit_Adress_Callback(hObject, eventdata)
@@ -185,6 +158,133 @@ end
 
 end % function
 
+
 % *************************************************************************
+function toggle_Connection_Callback(hObject, eventdata)
+
+handles = guidata(hObject);
+
+switch get(hObject,'Value')
+    
+    case 1
+        
+        recorderip = get(handles.edit_Adress,'String');
+        
+        fprintf('Trying to connect to : %s ... ',recorderip)
+        
+        % Establish connection to BrainVision Recorder Software 32Bit RDA-Port
+        % (use 51234 to connect with 16Bit Port)
+        con = pnet('tcpconnect', recorderip, 51244);
+        
+        % Check established connection and display a message
+        status = pnet(con,'status');
+        if status > 0
+            fprintf('connection established \n');
+        elseif status == -1
+            set(hObject,'Value',0)
+            error('connection FAILED')
+        end
+        
+        set(hObject,'BackgroundColor',[0.5 0.5 1])
+        set(handles.toggle_Stream,'Visible','On')
+        
+    case 0
+        
+        set(handles.toggle_Stream,'Value',0);
+        toggle_Stream_Callback(handles.toggle_Stream, eventdata)
+        
+        % Close all open socket connections
+        pnet('closeall');
+        
+        % Display a message
+        disp('connection closed');
+        
+        set(hObject,'BackgroundColor',handles.buttonBGcolor)
+        set(handles.toggle_Stream,'Visible','Off')
+        
+end
+
+end % function
 
 
+% *************************************************************************
+function toggle_Stream_Callback(hObject, eventdata)
+
+handles = guidata(hObject);
+
+switch get(hObject,'Value')
+    
+    case 1
+        
+        % Clear axes and ADC data
+        cla(handles.axes_Oscillo)
+        if isfield( handles , 'Scope' )
+            handles = rmfield( handles ,'Scope' );
+        end
+        
+        handles.RefreshPeriod = 0.020; % secondes
+        
+        handles.TimerHandle = timer(...
+            'StartDelay',0 ,...
+            'Period', handles.RefreshPeriod ,...
+            'TimerFcn', {@DoStream,handles.(mfilename)} ,...
+            'BusyMode', 'drop',...  %'queue'
+            'TasksToExecute', Inf,...
+            'ExecutionMode', 'fixedRate');
+        
+        guidata(hObject, handles);
+        
+        start(handles.TimerHandle)
+        
+        set(hObject,'BackgroundColor',[0.5 0.5 1])
+        
+    case 0
+        
+        if isfield(handles,'TimerHandle')
+            
+            try
+                stop( handles.TimerHandle );
+                delete( handles.TimerHandle );
+                handles = rmfield( handles ,'TimerHandle' );
+                handles = rmfield( handles ,'Scope' );
+            catch err
+                warning('GUI:Timer','Cannot delete the timer object. delete(timerfind) can clean all timers in the memory')
+            end
+            
+        end
+        
+        set(hObject,'BackgroundColor',handles.buttonBGcolor)
+        
+end
+
+guidata(hObject, handles);
+
+end % function
+
+
+% *************************************************************************
+function DoStream(hObject,eventdata,hFigure)
+
+handles = guidata(hFigure);
+
+% Create scope
+if ~isfield( handles , 'Scope' )
+    Window              = 10; % secondes
+    handles.Scope.EOGv  = nan( round(Window/handles.RefreshPeriod) , 1 );
+    handles.Scope.EOGh  = handles.Scope.EOGv;
+    handles.Time        = handles.RefreshPeriod : handles.RefreshPeriod : Window;
+    handles.L           = length(handles.Scope);
+    handles.Freq        = (0:(handles.L/2))/handles.L/handles.RefreshPeriod;
+end
+
+handles.Scope.EOGv  = circshift(handles.Scope.EOGv ,1);
+handles.Scope.EOGh  = circshift(handles.Scope.EOGh ,1);
+
+% Plot ADC
+plot( handles.axes_Oscillo  , handles.Time , handles.Scope.EOGv, 'blue'  );
+plot( handles.axes_Oscillo  , handles.Time , handles.Scope.EOGh, 'red'  );
+drawnow;
+
+guidata(handles.(mfilename), handles);
+
+end % function
